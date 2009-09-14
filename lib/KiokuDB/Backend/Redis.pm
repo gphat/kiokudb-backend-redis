30c99da4da5cde53d75f9f4b88910744c921c5d3
Cory Watson <gphat@Crankwizzah.local>
2009-09-14 15:14:58 -0500

    First commit, just boilder plate fttb

---
+++ lib/KiokuDB/Backend/Redis.pm
@@ -0,0 +1,110 @@
+package KiokuDB::Backend::Redis;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+KiokuDB::Backend::Redis - The great new KiokuDB::Backend::Redis!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use KiokuDB::Backend::Redis;
+
+    my $foo = KiokuDB::Backend::Redis->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Cory G Watson, C<< <gphat at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-kiokudb-backend-redis at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=KiokuDB-Backend-Redis>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc KiokuDB::Backend::Redis
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=KiokuDB-Backend-Redis>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/KiokuDB-Backend-Redis>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/KiokuDB-Backend-Redis>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/KiokuDB-Backend-Redis/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Cory G Watson.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of KiokuDB::Backend::Redis
