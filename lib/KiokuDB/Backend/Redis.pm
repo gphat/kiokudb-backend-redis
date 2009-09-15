f2f72ce5ed4073d1841146644279478f057b3a8c
Cory Watson <github@onemogin.com>
2009-09-15 14:03:02 -0500

    Passes tests lifted from BDB backend.

--- lib/KiokuDB/Backend/Redis.pm
+++ lib/KiokuDB/Backend/Redis.pm
@@ -1,20 +1,95 @@
 package KiokuDB::Backend::Redis;
+use Moose;
 
-use warnings;
-use strict;
-
-=head1 NAME
-
-KiokuDB::Backend::Redis - The great new KiokuDB::Backend::Redis!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
+use Redis;
 
 our $VERSION = '0.01';
 
+has '_redis' => (
+    is => 'ro',
+    isa => 'Redis',
+    lazy => 1,
+    default => sub {
+        Redis->new(server => '127.0.0.1:6379', debug => 0)
+    }
+);
+
+with qw(
+    KiokuDB::Backend
+    KiokuDB::Backend::Serialize::Delegate
+);
+
+sub delete {
+    my ($self, @ids_or_entries) = @_;
+
+    my $redis = $self->_redis;
+
+    my @uids = map { ref($_) ? $_->id : $_ } @ids_or_entries;
+
+    foreach my $id ( @uids ) {
+
+        $redis->del($id);
+    }
+
+    return;
+}
+
+sub exists {
+    my ($self, @ids) = @_;
+
+    my @exists;
+
+    my $redis = $self->_redis;
+    foreach my $id (@ids) {
+
+        if($redis->exists($id)) {
+            push(@exists, 1);
+        } else {
+            push(@exists, 0);
+        }
+    }
+
+    return @exists;
+}
+
+sub insert {
+    my ($self, @entries) = @_;
+
+    my $redis = $self->_redis;
+
+    foreach my $entry ( @entries ) {
+        my $ret = $redis->set(
+            $entry->id => $self->serialize($entry),
+        );
+    }
+}
+
+sub get {
+    my ($self, @ids) = @_;
+
+    my ( $var, @ret );
+
+    my $redis = $self->_redis;
+
+    foreach my $id ( @ids ) {
+        my $val = $redis->get($id);
+        if(defined($val)) {
+            push @ret, $val;
+        } else {
+            return;
+        }
+    }
+
+    return map { $self->deserialize($_) } @ret;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+KiokuDB::Backend::Redis - Redis backend for KiokuDB
 
 =head1 SYNOPSIS
 
@@ -27,27 +102,6 @@ Perhaps a little code snippet.
     my $foo = KiokuDB::Backend::Redis->new();
     ...
 
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 FUNCTIONS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
-
 =head1 AUTHOR
 
 Cory G Watson, C<< <gphat at cpan.org> >>
@@ -57,9 +111,6 @@ Cory G Watson, C<< <gphat at cpan.org> >>
 Please report any bugs or feature requests to C<bug-kiokudb-backend-redis at rt.cpan.org>, or through
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=KiokuDB-Backend-Redis>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
-
-
-
 
 =head1 SUPPORT
 
--- t/00-load.t
+++ t/00-load.t
@@ -1,9 +1,9 @@
-#!perl -T
-
-use Test::More tests => 1;
+use Test::More;
 
 BEGIN {
     use_ok( 'KiokuDB::Backend::Redis' );
 }
 
 diag( "Testing KiokuDB::Backend::Redis $KiokuDB::Backend::Redis::VERSION, Perl $], $^X" );
+
+done_testing;
